a3599a15b812f77a5cbd58bc7d83ee7e91cd81f1
Ryota Ono <ryota.ono@aitokyolab.com>
2017-05-14 17:05:10 +0900

    rankingスコアの計算をする関数を作成

---
+++ src/Ranking.hs
@@ -0,0 +1,32 @@
+import Data.Map
+
+--average list = realToFrac (sum list) /  genericLength list
+average xs = (sum xs) / (fromIntegral (length xs))
+ 
+ranking xs =
+        let avg = average xs
+            len = fromIntegral (length xs)
+            res1 = avg + len
+        in res1
+
+calc_each_rank xs = 
+        let item_id = fst xs
+            score = ranking (Prelude.map snd (snd xs))
+        in (item_id,score)
+
+pack_rank_result [] = [] 
+pack_rank_result (x:xs) = [calc_each_rank x] ++ (pack_rank_result xs) 
+
+--main = do
+--    let
+--        score_map = fromList [(1001,[(1,5),(2,4),(3,3)]),(1002,[(1,2),(2,3),(3,3)])]
+--        score_list = toList score_map
+--        l = head score_list
+----        res = pack_rank_result score_list
+--        res = Prelude.map calc_each_rank score_list
+--{-
+--        p = Prelude.map snd score_list
+--        b = ranking p
+---}
+--        -- p = ranking days
+--    print res
